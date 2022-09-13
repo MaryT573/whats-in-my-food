99d073b92e3847d44376fe15c337e7234b5f06b9
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-13 00:44:52 -0600

    more assertions to service test

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe FoodService do
     it 'gets data from api' do
         foods = FoodService.search_food('cheese') 
- 
-        expect(foods).to be_a(Hash)      
+        expect(foods).to be_a(Hash) 
+        expect(foods[:foods]).to be_a(Array)
+        
+        food = foods[:foods].first
+        
+        expect(food[:description]).to be_a(String)
+        expect(food[:fdcId]).to be_a(Integer)
+        expect(food[:brandOwner]).to be_a(String)
+        expect(food[:ingredients]).to be_a(String)
     end
 end
